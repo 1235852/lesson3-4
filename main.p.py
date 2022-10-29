4adab43197512856fa6148e87ef3564d85f0fc30
Студент <student@ITStep.local>
2022-10-29 16:25:23 +0300

    Initial commit

---
+++ main.p.py
@@ -0,0 +1,27 @@
+import random
+class Human:
+    def __init__(self, name = "Негр Негравич", job = None,home = None,):
+        self.name = name
+        self.job = job
+        self.home = home
+        self.car= car
+        self.money = 0
+        def get_home(self):
+            pass
+        def get_car(self):
+            pass
+        def get_job(self):
+            pass
+        def food(self):
+            pass
+        def work(self):
+            pass
+        def shopping(self):
+            pass
+        def chil(self):
+            pass
+        def live(self, day):
+            pass
+
+class Auto:
+    def __init__(self, brand):
